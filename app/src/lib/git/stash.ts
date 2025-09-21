thangckt/github-desktop-plus

commit eed0344b5697247de9102172588062b6991c12a1

Impl/stash selected changes
Squashed changes from this PR:
https://github.com/desktop/desktop/pull/16535

app/src/lib/git/stash.ts
@@ -155,7 +155,7 @@ export async function createDesktopStashEntry(
 
   const branchName = typeof branch === 'string' ? branch : branch.name
   const message = createDesktopStashMessage(branchName)
-  const args = ['stash', 'push', '-m', message]
+  const args = ['stash', 'push', '-m', message, ...fullySelectedUntrackedFiles.map(x => x.path)]
 
   const result = await git(args, repository.path, 'createStashEntry', {
     successExitCodes: new Set<number>([0, 1]),

app/src/lib/stores/app-store.ts
@@ -289,6 +289,7 @@ import {
   ErrorWithMetadata,
   CheckoutError,
   DiscardChangesError,
+  StashChangesError,
 } from '../error-with-metadata'
 import {
   ShowSideBySideDiffDefault,
@@ -377,6 +378,7 @@ const confirmRepoRemovalDefault: boolean = true
 const showCommitLengthWarningDefault: boolean = false
 const confirmDiscardChangesDefault: boolean = true
 const confirmDiscardChangesPermanentlyDefault: boolean = true
+const confirmStashChangesDefault: boolean = true
 const confirmDiscardStashDefault: boolean = true
 const confirmCheckoutCommitDefault: boolean = true
 const askForConfirmationOnForcePushDefault = true
@@ -385,6 +387,7 @@ const askToMoveToApplicationsFolderKey: string = 'askToMoveToApplicationsFolder'
 const confirmRepoRemovalKey: string = 'confirmRepoRemoval'
 const showCommitLengthWarningKey: string = 'showCommitLengthWarning'
 const confirmDiscardChangesKey: string = 'confirmDiscardChanges'
+const confirmStashChangesKey: string = 'confirmStashChanges'
 const confirmDiscardStashKey: string = 'confirmDiscardStash'
 const confirmCheckoutCommitKey: string = 'confirmCheckoutCommit'
 const confirmDiscardChangesPermanentlyKey: string =
@@ -517,6 +520,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
   private confirmDiscardChanges: boolean = confirmDiscardChangesDefault
   private confirmDiscardChangesPermanently: boolean =
     confirmDiscardChangesPermanentlyDefault
+  private confirmStashChanges: boolean = confirmStashChangesDefault
   private confirmDiscardStash: boolean = confirmDiscardStashDefault
   private confirmCheckoutCommit: boolean = confirmCheckoutCommitDefault
   private askForConfirmationOnForcePush = askForConfirmationOnForcePushDefault
@@ -1044,6 +1048,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
       askForConfirmationOnDiscardChanges: this.confirmDiscardChanges,
       askForConfirmationOnDiscardChangesPermanently:
         this.confirmDiscardChangesPermanently,
+      askForConfirmationOnStashChanges: this.confirmStashChanges,
       askForConfirmationOnDiscardStash: this.confirmDiscardStash,
       askForConfirmationOnCheckoutCommit: this.confirmCheckoutCommit,
       askForConfirmationOnForcePush: this.askForConfirmationOnForcePush,
@@ -2198,6 +2203,11 @@ export class AppStore extends TypedBaseStore<IAppState> {
     this.confirmDiscardChangesPermanently = getBoolean(
       confirmDiscardChangesPermanentlyKey,
       confirmDiscardChangesPermanentlyDefault
+    )
+
+    this.confirmStashChanges = getBoolean(
+      confirmStashChangesKey,
+      confirmStashChangesDefault
     )
 
     this.confirmDiscardStash = getBoolean(
@@ -5000,6 +5010,32 @@ export class AppStore extends TypedBaseStore<IAppState> {
     return this._refreshRepository(repository)
   }
 
+  public async _stashChanges(
+    repository: Repository,
+    files: ReadonlyArray<WorkingDirectoryFileChange>
+  ) {
+    try {
+      const repositoryState = this.repositoryStateCache.get(repository)
+      const tip = repositoryState.branchesState.tip
+      const currentBranch = tip.kind === TipState.Valid ? tip.branch : null
+
+      if (currentBranch === null) {
+        return
+      }
+      
+      await this.createStashEntries(repository, currentBranch, files)
+    } catch (error) {
+      if (!(error instanceof StashChangesError)) {
+        log.error('Failed stashing changes', error)
+      }
+
+      this.emitError(error)
+      return
+    }
+
+    return this._refreshRepository(repository)
+  }
+
   public async _discardChangesFromSelection(
     repository: Repository,
     filePath: string,
@@ -5752,6 +5788,15 @@ export class AppStore extends TypedBaseStore<IAppState> {
     this.confirmDiscardChanges = value
 
     setBoolean(confirmDiscardChangesKey, value)
+    this.emitUpdate()
+
+    return Promise.resolve()
+  }
+
+  public _setConfirmStashChangesSetting(value: boolean): Promise<void> {
+    this.confirmStashChanges = value
+
+    setBoolean(confirmStashChangesKey, value)
     this.emitUpdate()
 
     return Promise.resolve()
@@ -6875,11 +6920,34 @@ export class AppStore extends TypedBaseStore<IAppState> {
   }
 
   private async createStashEntry(repository: Repository, branch: Branch) {
+    const stashEntry = await getLastDesktopStashEntryForBranch(repository, branch)
+    
+    if (stashEntry !== null) {
+      await this._popStashEntry(repository, stashEntry)
+    }
+
     const { changesState } = this.repositoryStateCache.get(repository)
     const { workingDirectory } = changesState
     const untrackedFiles = getUntrackedFiles(workingDirectory)
 
     return createDesktopStashEntry(repository, branch, untrackedFiles)
+  }
+
+  private async createStashEntries(repository: Repository, branch: Branch, files: ReadonlyArray<WorkingDirectoryFileChange>) {
+    const { changesState } = this.repositoryStateCache.get(repository)
+    const { workingDirectory } = changesState
+
+    const stashEntry = await getLastDesktopStashEntryForBranch(repository, branch)
+    
+    if (stashEntry !== null) {
+      await this._popStashEntry(repository, stashEntry)
+    }
+
+    const newChangesState = this.repositoryStateCache.get(repository).changesState
+    const newWorkingDirectory = newChangesState.workingDirectory
+    const stashPoppedFiles = newWorkingDirectory.files.filter(stashFile => workingDirectory.files.findIndex(file => file.path === stashFile.path) === -1)
+
+    return createDesktopStashEntry(repository, branch, [...files, ...stashPoppedFiles])
   }
 
   /** This shouldn't be called directly. See `Dispatcher`. */

app/src/lib/app-state.ts
@@ -219,6 +219,9 @@ export interface IAppState {
   readonly askForConfirmationOnDiscardChanges: boolean
 
   /** Whether we should show a confirmation dialog */
+  readonly askForConfirmationOnStashChanges: boolean
+
+  /** Whether we should show a confirmation dialog */
   readonly askForConfirmationOnDiscardChangesPermanently: boolean
 
   /** Should the app prompt the user to confirm a discard stash */

app/src/lib/error-with-metadata.ts
@@ -66,3 +66,19 @@ export class DiscardChangesError extends ErrorWithMetadata {
     })
   }
 }
+
+/**
+ * An error thrown when a failure occurs while stashing changes.
+ * Technically just a convenience class on top of ErrorWithMetadata
+ */
+export class StashChangesError extends ErrorWithMetadata {
+  public constructor(
+    error: Error,
+    repository: Repository,
+    files: ReadonlyArray<WorkingDirectoryFileChange>
+  ) {
+    super(error, {
+      retryAction: { type: RetryActionType.StashChanges, files, repository },
+    })
+  }
+}

app/src/models/popup.ts
@@ -29,6 +29,7 @@ export enum PopupType {
   DeleteBranch = 'DeleteBranch',
   DeleteRemoteBranch = 'DeleteRemoteBranch',
   ConfirmDiscardChanges = 'ConfirmDiscardChanges',
+  ConfirmStashChanges = 'ConfirmStashChanges',
   Preferences = 'Preferences',
   RepositorySettings = 'RepositorySettings',
   AddRepository = 'AddRepository',
@@ -125,6 +126,13 @@ export type PopupDetail =
       showDiscardChangesSetting?: boolean
       discardingAllChanges?: boolean
       permanentlyDelete?: boolean
+    }
+  | {
+      type: PopupType.ConfirmStashChanges
+      repository: Repository
+      files: ReadonlyArray<WorkingDirectoryFileChange>
+      showStashChangesSetting?: boolean
+      stashingAllChanges?: boolean
     }
   | {
       type: PopupType.ConfirmDiscardSelection

app/src/models/retry-actions.ts
@@ -18,6 +18,7 @@ export enum RetryActionType {
   Squash,
   Reorder,
   DiscardChanges,
+  StashChanges,
 }
 
 /** The retriable actions and their associated data. */
@@ -82,6 +83,11 @@ export type RetryAction =
     }
   | {
       type: RetryActionType.DiscardChanges
+      repository: Repository
+      files: ReadonlyArray<WorkingDirectoryFileChange>
+    }
+  | {
+      type: RetryActionType.StashChanges
       repository: Repository
       files: ReadonlyArray<WorkingDirectoryFileChange>
     }

app/src/ui/app.tsx
@@ -177,6 +177,7 @@ import { TestNotifications } from './test-notifications/test-notifications'
 import { NotificationsDebugStore } from '../lib/stores/notifications-debug-store'
 import { PullRequestComment } from './notifications/pull-request-comment'
 import { UnknownAuthors } from './unknown-authors/unknown-authors-dialog'
+import { StashChanges } from './stash-changes/stash-changes-dialog'
 import { UnsupportedOSBannerDismissedAtKey } from './banners/os-version-no-longer-supported-banner'
 import { offsetFromNow } from '../lib/offset-from'
 import { getBoolean, getNumber } from '../lib/local-storage'
@@ -1700,6 +1701,29 @@ export class App extends React.Component<IAppProps, IAppState> {
             onConfirmDiscardChangesChanged={this.onConfirmDiscardChangesChanged}
           />
         )
+      case PopupType.ConfirmStashChanges:
+        const showStashChangesSetting =
+          popup.showStashChangesSetting === undefined
+            ? true
+            : popup.showStashChangesSetting
+        const stashingAllChanges =
+          popup.stashingAllChanges === undefined
+            ? false
+            : popup.stashingAllChanges
+
+        return (
+          <StashChanges
+            key="stash-changes"
+            repository={popup.repository}
+            dispatcher={this.props.dispatcher}
+            files={popup.files}
+            confirmStashChanges={this.state.askForConfirmationOnStashChanges}
+            showStashChangesSetting={showStashChangesSetting}
+            stashingAllChanges={stashingAllChanges}
+            onDismissed={onPopupDismissedFn}
+            onConfirmStashChangesChanged={this.onConfirmStashChangesChanged}
+          />
+        )
       case PopupType.ConfirmDiscardSelection:
         return (
           <DiscardSelection
@@ -2822,6 +2846,10 @@ export class App extends React.Component<IAppProps, IAppState> {
 
   private onConfirmDiscardChangesChanged = (value: boolean) => {
     this.props.dispatcher.setConfirmDiscardChangesSetting(value)
+  }
+
+  private onConfirmStashChangesChanged = (value: boolean) => {
+    this.props.dispatcher.setConfirmStashChangesSetting(value)
   }
 
   private onConfirmDiscardChangesPermanentlyChanged = (value: boolean) => {

app/src/ui/changes/changes-list.tsx
@@ -128,6 +128,10 @@ interface IChangesListProps {
     isDiscardingAllChanges: boolean,
     permanently: boolean
   ) => void
+  readonly onStashChangesFromFiles: (
+    files: ReadonlyArray<WorkingDirectoryFileChange>,
+    isStashingAllChanges: boolean
+  ) => void
 
   /** Callback that fires on page scroll to pass the new scrollTop location */
   readonly onChangesListScrolled: (scrollTop: number) => void
@@ -328,6 +332,10 @@ export class ChangesList extends React.Component<
     )
   }
 
+  private onStashAllChanges = () => {
+    this.props.dispatcher.createStashForCurrentBranch(this.props.repository)
+  }
+  
   private onDiscardAllChanges = () => {
     this.props.onDiscardChangesFromFiles(
       this.props.workingDirectory.files,
@@ -344,8 +352,25 @@ export class ChangesList extends React.Component<
     )
   }
 
-  private onStashChanges = () => {
-    this.props.dispatcher.createStashForCurrentBranch(this.props.repository)
+  private onStashChanges = (files: ReadonlyArray<string>) => {
+    const workingDirectory = this.props.workingDirectory
+    const modifiedFiles = new Array<WorkingDirectoryFileChange>()
+
+    files.forEach(file => {
+      const modifiedFile = workingDirectory.files.find(f => f.path === file)
+
+      if (modifiedFile != null) {
+        modifiedFiles.push(modifiedFile)
+      }
+    })
+
+    const stashingAllChanges =
+          modifiedFiles.length === workingDirectory.files.length
+
+    this.props.onStashChangesFromFiles(
+      modifiedFiles,
+      stashingAllChanges
+    )
   }
 
   private onDiscardChanges = (files: ReadonlyArray<string>) => {
@@ -396,6 +421,19 @@ export class ChangesList extends React.Component<
     return this.props.askForConfirmationOnDiscardChanges ? `${label}…` : label
   }
 
+  private getStashChangesMenuItemLabel = (files: ReadonlyArray<string>) => {
+    const label =
+      files.length === 1
+        ? __DARWIN__
+          ? `Stash Changes`
+          : `Stash changes`
+        : __DARWIN__
+        ? `Stash ${files.length} Selected Changes`
+        : `Stash ${files.length} selected changes`
+
+    return this.props.askForConfirmationOnDiscardChanges ? `${label}…` : label
+  }
+
   private onContextMenu = (event: React.MouseEvent<any>) => {
     event.preventDefault()
 
@@ -432,7 +470,7 @@ export class ChangesList extends React.Component<
       },
       {
         label: hasStash ? confirmStashAllChangesLabel : stashAllChangesLabel,
-        action: this.onStashChanges,
+        action: this.onStashAllChanges,
         enabled: hasLocalChanges && this.props.branch !== null && !hasConflicts,
       },
     ]
@@ -446,6 +484,15 @@ export class ChangesList extends React.Component<
     return {
       label: this.getDiscardChangesMenuItemLabel(paths),
       action: () => this.onDiscardChanges(paths),
+    }
+  }
+
+  private getStashChangesMenuItem = (
+    paths: ReadonlyArray<string>
+  ): IMenuItem => {
+    return {
+      label: this.getStashChangesMenuItemLabel(paths),
+      action: () => this.onStashChanges(paths),
     }
   }
 
@@ -564,6 +611,7 @@ export class ChangesList extends React.Component<
 
     const items: IMenuItem[] = [
       this.getDiscardChangesMenuItem(paths),
+      this.getStashChangesMenuItem(paths),
       { type: 'separator' },
     ]
     if (paths.length === 1) {

desktop-plus/desktop-plus

commit 5d71b53272b9c06877f3c47a6badf5ef8c793106

Fix bug where "Stash all" would miss some files
If the worktree contains untracked files, only those files would be stashed, leaving all other files unstashed.
This bug was introduced in eed0344b5697247de9102172588062b6991c12a1

app/src/lib/git/stash.ts
@@ -142,7 +142,8 @@ export function createDesktopStashMessage(branchName: string) {
 export async function createDesktopStashEntry(
   repository: Repository,
   branch: Branch | string,
-  untrackedFilesToStage: ReadonlyArray<WorkingDirectoryFileChange>
+  untrackedFilesToStage: ReadonlyArray<WorkingDirectoryFileChange>,
+  isStashAllChanges: boolean
 ): Promise<boolean> {
   // We must ensure that no untracked files are present before stashing
   // See https://github.com/desktop/desktop/pull/8085
@@ -155,13 +156,10 @@ export async function createDesktopStashEntry(
 
   const branchName = typeof branch === 'string' ? branch : branch.name
   const message = createDesktopStashMessage(branchName)
-  const args = [
-    'stash',
-    'push',
-    '-m',
-    message,
-    ...fullySelectedUntrackedFiles.map(x => x.path),
-  ]
+  const args = ['stash', 'push', '-m', message]
+  if (!isStashAllChanges) {
+    args.push(...fullySelectedUntrackedFiles.map(x => x.path))
+  }
 
   const result = await git(args, repository.path, 'createStashEntry').catch(
     e => {

app/src/lib/stores/app-store.ts
@@ -7314,7 +7314,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
     const { workingDirectory } = changesState
     const untrackedFiles = getUntrackedFiles(workingDirectory)
 
-    return createDesktopStashEntry(repository, branch, untrackedFiles)
+    return createDesktopStashEntry(repository, branch, untrackedFiles, true)
   }
 
   private async createStashEntries(
@@ -7344,10 +7344,12 @@ export class AppStore extends TypedBaseStore<IAppState> {
         ) === -1
     )
 
-    return createDesktopStashEntry(repository, branch, [
-      ...files,
-      ...stashPoppedFiles,
-    ])
+    return createDesktopStashEntry(
+      repository,
+      branch,
+      [...files, ...stashPoppedFiles],
+      false
+    )
   }
 
   /** This shouldn't be called directly. See `Dispatcher`. */

app/test/unit/git/stash-test.ts
@@ -74,7 +74,7 @@ describe('git/stash', () => {
         'just testing stuff'
       )
 
-      await createDesktopStashEntry(repository, 'master', [])
+      await createDesktopStashEntry(repository, 'master', [], true)
 
       const stash = await getStashes(repository)
       const entries = stash.desktopEntries
@@ -98,7 +98,7 @@ describe('git/stash', () => {
         f => f.status.kind === AppFileStatusKind.Untracked
       )
 
-      await createDesktopStashEntry(repository, 'master', untrackedFiles)
+      await createDesktopStashEntry(repository, 'master', untrackedFiles, true)
 
       status = await getStatusOrThrow(repository)
       files = status.workingDirectory.files