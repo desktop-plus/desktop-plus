3307a5b26dc10e689f79ffcc79dd36351396960c
Pol Rivero <65060696+pol-rivero@users.noreply.github.com>
2026-09-10 20:40:19 +0200

    Don't hardcode 'api/v3' in getAbsoluteUrl
    v3 is only correct for GitHub. GitLab, Gitea and Forgejo use different numbers.
    
    Fixes #259

--- app/src/lib/http.ts
+++ app/src/lib/http.ts
@@ -75,6 +75,8 @@ async function deserialize<T>(response: Response): Promise<T> {
   }
 }
 
+const StripApiVersionPrefixRegex = /^api\/v\d+\//g
+
 /**
  * Convert the endpoint and resource path into an absolute URL. As the app bakes
  * the `/api/v3/` path into the endpoint, we need to prevent duplicating this when
@@ -85,9 +87,7 @@ async function deserialize<T>(response: Response): Promise<T> {
  */
 export function getAbsoluteUrl(endpoint: string, path: string): string {
   let relativePath = path[0] === '/' ? path.substring(1) : path
-  if (relativePath.startsWith('api/v3/')) {
-    relativePath = relativePath.substring(7)
-  }
+  relativePath = relativePath.replace(StripApiVersionPrefixRegex, '')
 
   // Our API endpoints are a bit sloppy in that they don't typically
   // include the trailing slash (i.e. we use https://api.github.com for
